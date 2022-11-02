97d967f4e720ad27541fc3494a1872aa9fa3e423
최지연 <choi02647@naver.com>
2022-11-02 18:24:01 +0900

    ui 추가

--- COVID19/ViewController.swift
+++ COVID19/ViewController.swift
@@ -6,14 +6,16 @@
 //
 
 import UIKit
+import Charts
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var totalCaseLabel: UILabel!
+    @IBOutlet weak var newCaseLabel: UILabel!
+    @IBOutlet weak var pieChartView: PieChartView!
+    @IBOutlet weak var labelStackView: UIStackView!
+    @IBOutlet weak var indicatorView: UIActivityIndicatorView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
     }
-
-
 }
-
