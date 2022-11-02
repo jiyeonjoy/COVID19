3c082c63d75d2b93f0c6ad3a3b7250205dbe68e4
최지연 <choi02647@naver.com>
2022-11-02 18:49:09 +0900

    api 연동 추가

--- COVID19/ViewController.swift
+++ COVID19/ViewController.swift
@@ -6,7 +6,9 @@
 //
 
 import UIKit
+
 import Charts
+import Alamofire
 
 class ViewController: UIViewController {
     @IBOutlet weak var totalCaseLabel: UILabel!
@@ -17,5 +19,39 @@ class ViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        self.fetchCovidOverview(completionHandler: { [weak self] result in
+            guard let self = self else { return }
+            switch result {
+            case let .success(result):
+                debugPrint("success \(result)")
+            case let .failure(error):
+                debugPrint("error \(error)")
+            }
+        })
+    }
+    
+    func fetchCovidOverview(
+        /// 함수를 탈출해도 호출가능! 비동기 작업인 경우 해줘야됨. 언제 api 통신이 완료될지 모르기 때문!
+      completionHandler: @escaping (Result<CityCovidOverview, Error>) -> Void
+    ) {
+      let url = "https://api.corona-19.kr/korea/country/new/"
+      let param = [
+        "serviceKey": "MHym6cezIlKtFRN3Wr2n4ExpYUw5fiJaD"
+      ]
+      AF.request(url, method: .get, parameters: param)
+        .responseData(completionHandler: { response in
+          switch response.result {
+          case let .success(data):
+            do {
+              let decoder = JSONDecoder()
+              let result = try decoder.decode(CityCovidOverview.self, from: data)
+              completionHandler(.success(result))
+            } catch {
+              completionHandler(.failure(error))
+            }
+          case let .failure(error):
+            completionHandler(.failure(error))
+          }
+        })
     }
 }
