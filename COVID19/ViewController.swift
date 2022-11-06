79573a5740e07d40821146c284e120e611104f48
jiyeonchoi <choi02647@naver.com>
2022-11-06 21:48:14 +0900

    pie chart done

--- COVID19/ViewController.swift
+++ COVID19/ViewController.swift
@@ -21,13 +21,80 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         self.fetchCovidOverview(completionHandler: { [weak self] result in
             guard let self = self else { return }
+            self.indicatorView.stopAnimating()
+            self.indicatorView.isHidden = true
+            self.labelStackView.isHidden = false
+            self.pieChartView.isHidden = false
             switch result {
             case let .success(result):
-                debugPrint("success \(result)")
+                self.configureStackView(koreaCovidOverview: result.korea)
+                let covidOverviewList = self.makeCovidOverviewList(cityCovidOverview: result)
+                self.configureChartView(covidOverviewList: covidOverviewList)
             case let .failure(error):
                 debugPrint("error \(error)")
             }
         })
+    }
+    
+    func makeCovidOverviewList(cityCovidOverview: CityCovidOverview) -> [CovidOverview] {
+      return [
+        cityCovidOverview.seoul,
+        cityCovidOverview.busan,
+        cityCovidOverview.daegu,
+        cityCovidOverview.incheon,
+        cityCovidOverview.gwangju,
+        cityCovidOverview.daejeon,
+        cityCovidOverview.ulsan,
+        cityCovidOverview.sejong,
+        cityCovidOverview.gyeonggi,
+        cityCovidOverview.chungbuk,
+        cityCovidOverview.chungnam,
+        cityCovidOverview.jeonnam,
+        cityCovidOverview.gyeongbuk,
+        cityCovidOverview.gyeongnam,
+        cityCovidOverview.jeju,
+      ]
+    }
+
+    func configureStackView(koreaCovidOverview: CovidOverview) {
+      self.totalCaseLabel.text = "\(koreaCovidOverview.totalCase) 명"
+      self.newCaseLabel.text = "\(koreaCovidOverview.newCase) 명"
+    }
+
+    func configureChartView(covidOverviewList: [CovidOverview]) {
+      self.pieChartView.delegate = self
+      let entries = covidOverviewList.compactMap { [weak self] overview -> PieChartDataEntry? in
+        guard let self = self else { return nil }
+        return PieChartDataEntry(
+          value: self.removeFormatString(string: overview.newCase),
+          label: overview.countryName,
+          data: overview
+        )
+      }
+      let dataSet = PieChartDataSet(entries: entries, label: "코로나 발생 현황")
+      dataSet.sliceSpace = 1
+      dataSet.entryLabelColor = .black
+      dataSet.xValuePosition = .outsideSlice
+      dataSet.valueTextColor = .black
+      dataSet.valueLinePart1OffsetPercentage = 0.8
+      dataSet.valueLinePart1Length = 0.2
+      dataSet.valueLinePart2Length = 0.3
+
+      dataSet.colors = ChartColorTemplates.vordiplom()
+        + ChartColorTemplates.joyful()
+        + ChartColorTemplates.colorful()
+        + ChartColorTemplates.liberty()
+        + ChartColorTemplates.pastel()
+        + ChartColorTemplates.material()
+
+      self.pieChartView.data = PieChartData(dataSet: dataSet)
+      self.pieChartView.spin(duration: 0.3, fromAngle: pieChartView.rotationAngle, toAngle: pieChartView.rotationAngle + 80)
+    }
+
+    func removeFormatString(string: String) -> Double {
+      let formatter = NumberFormatter()
+      formatter.numberStyle = .decimal
+      return formatter.number(from: string)?.doubleValue ?? 0
     }
     
     func fetchCovidOverview(
@@ -54,4 +121,15 @@ class ViewController: UIViewController {
           }
         })
     }
+}
+
+extension ViewController: ChartViewDelegate {
+  func chartValueSelected(_ chartView: ChartViewBase, entry: ChartDataEntry, highlight: Highlight) {
+    guard let covidDetailViewController = self.storyboard?.instantiateViewController(identifier: "CovidDetailViewController") as? CovidDetailViewController else {
+         return
+       }
+    guard let covidOverview = entry.data as? CovidOverview else { return }
+    covidDetailViewController.covidOverview = covidOverview
+    self.navigationController?.pushViewController(covidDetailViewController, animated: true)
+  }
 }
